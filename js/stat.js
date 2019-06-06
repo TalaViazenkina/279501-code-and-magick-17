239fc9f62705f59bd5070b27cf9e3e0fa8e68614
Natallia Viazenkina <natala.viaz@gmail.com>
2019-06-06 15:59:39 +0300

    Добавлена поздравительная надпись на облаке

--- js/stat.js
+++ js/stat.js
@@ -2,6 +2,9 @@
 
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
+var CLOUD_FONT = 16;
+var CLOUD_LINE_HEIGHT = CLOUD_FONT * 1.2;
+var GAP = 50;
 
 // функция отрисовки облака
 var renderCloud = function (ctx, x, y, color) {
@@ -13,4 +16,12 @@ var renderCloud = function (ctx, x, y, color) {
 window.renderStatistics = function (ctx) {
   renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7');
   renderCloud(ctx, 100, 10, '#ffffff');
+
+// поздравительная надпись на облаке
+  ctx.fillStyle = '#000000';
+  ctx.font = CLOUD_FONT + 'px "PT Mono"';
+  ctx.textAlign = 'center';
+  ctx.textBaseline = 'top';
+  ctx.fillText('Ура вы победили!', 100 + CLOUD_WIDTH / 2, GAP / 2);
+  ctx.fillText('Список результатов:', 100 + CLOUD_WIDTH / 2, GAP / 2 + CLOUD_LINE_HEIGHT);
 };
