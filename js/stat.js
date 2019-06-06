35358d68b8a23f83352f6b19ec95c458323f5115
Natallia Viazenkina <natala.viaz@gmail.com>
2019-06-06 20:30:35 +0300

    Переменные разделены на константы и вычисляемые переменные

--- js/stat.js
+++ js/stat.js
@@ -1,16 +1,16 @@
 'use strict';
 
-var CLOUD_WIDTH = 420;
-var CLOUD_HEIGHT = 270;
-var CLOUD_X = 100;
-var CLOUD_Y = 10;
-var CLOUD_FONT = 16;
-var CLOUD_LINE_HEIGHT = CLOUD_FONT * 1.2;
-var GAP = 50;
-var STAT_X = CLOUD_X + GAP * 1.1;
-var STAT_Y = CLOUD_Y + GAP * 1.5;
-var STAT_BAR_WIDTH = 40;
-var STAT_BAR_MAX_HEIGHT = 150;
+var CLOUD_WIDTH = 420; // ширина всплывающего окна
+var CLOUD_HEIGHT = 270; // высота всплывающего окна
+var CLOUD_X = 100; // координата Х всплывающего окна
+var CLOUD_Y = 10; // координата Y всплывающего окна
+var CLOUD_FONT = 16; // размер шрифта
+var cloudLineHeight = CLOUD_FONT * 1.2; // интерлиньяж
+var GAP = 50; // отступы между столбцами
+var statX = CLOUD_X + GAP * 1.1; // координата Х блока со статистикой
+var statY = CLOUD_Y + GAP * 1.5; // координата Y блока со статистикой
+var STAT_BAR_WIDTH = 40; // ширина столбца гистограммы
+var STAT_BAR_MAX_HEIGHT = 150; // максимальная высота столбца гистограммы
 
 // функция отрисовки облака
 var renderCloud = function (ctx, x, y, color) {
@@ -28,30 +28,30 @@ window.renderStatistics = function (ctx) {
   ctx.font = CLOUD_FONT + 'px PT Mono';
   ctx.textAlign = 'center';
   ctx.fillText('Ура вы победили!', CLOUD_X + CLOUD_WIDTH / 2, CLOUD_Y + GAP / 2);
-  ctx.fillText('Список результатов:', CLOUD_X + CLOUD_WIDTH / 2, CLOUD_Y + GAP / 2 + CLOUD_LINE_HEIGHT);
+  ctx.fillText('Список результатов:', CLOUD_X + CLOUD_WIDTH / 2, CLOUD_Y + GAP / 2 + cloudLineHeight);
 
   // гистограмма
   ctx.textAlign = 'left';
   // ctx.textBaseline = 'top';
-  ctx.fillText('150', STAT_X, STAT_Y);
-  ctx.fillRect(STAT_X, STAT_Y + CLOUD_LINE_HEIGHT / 2, STAT_BAR_WIDTH, STAT_BAR_MAX_HEIGHT);
-  ctx.fillText('Вы', STAT_X, STAT_Y + STAT_BAR_MAX_HEIGHT + CLOUD_LINE_HEIGHT * 1.5);
+  ctx.fillText('150', statX, statY);
+  ctx.fillRect(statX, statY + cloudLineHeight / 2, STAT_BAR_WIDTH, STAT_BAR_MAX_HEIGHT);
+  ctx.fillText('Вы', statX, statY + STAT_BAR_MAX_HEIGHT + cloudLineHeight * 1.5);
 
   // добавили ещё победителей
   // игрок 2
-  ctx.fillText('150', STAT_X + (STAT_BAR_WIDTH + GAP), STAT_Y);
-  ctx.fillRect(STAT_X + (STAT_BAR_WIDTH + GAP), STAT_Y + CLOUD_LINE_HEIGHT / 2, STAT_BAR_WIDTH, STAT_BAR_MAX_HEIGHT);
-  ctx.fillText('Игрок2', STAT_X + (STAT_BAR_WIDTH + GAP), STAT_Y + STAT_BAR_MAX_HEIGHT + CLOUD_LINE_HEIGHT * 1.5);
+  ctx.fillText('150', statX + (STAT_BAR_WIDTH + GAP), statY);
+  ctx.fillRect(statX + (STAT_BAR_WIDTH + GAP), statY + cloudLineHeight / 2, STAT_BAR_WIDTH, STAT_BAR_MAX_HEIGHT);
+  ctx.fillText('Игрок2', statX + (STAT_BAR_WIDTH + GAP), statY + STAT_BAR_MAX_HEIGHT + cloudLineHeight * 1.5);
 
 
   // игрок 3
-  ctx.fillText('150', STAT_X + (STAT_BAR_WIDTH + GAP) * 2, STAT_Y);
-  ctx.fillRect(STAT_X + (STAT_BAR_WIDTH + GAP) * 2, STAT_Y + CLOUD_LINE_HEIGHT / 2, STAT_BAR_WIDTH, STAT_BAR_MAX_HEIGHT);
-  ctx.fillText('Игрок3', STAT_X + (STAT_BAR_WIDTH + GAP) * 2, STAT_Y + STAT_BAR_MAX_HEIGHT + CLOUD_LINE_HEIGHT * 1.5);
+  ctx.fillText('150', statX + (STAT_BAR_WIDTH + GAP) * 2, statY);
+  ctx.fillRect(statX + (STAT_BAR_WIDTH + GAP) * 2, statY + cloudLineHeight / 2, STAT_BAR_WIDTH, STAT_BAR_MAX_HEIGHT);
+  ctx.fillText('Игрок3', statX + (STAT_BAR_WIDTH + GAP) * 2, statY + STAT_BAR_MAX_HEIGHT + cloudLineHeight * 1.5);
 
 
   // игрок 4
-  ctx.fillText('150', STAT_X + (STAT_BAR_WIDTH + GAP) * 3, STAT_Y);
-  ctx.fillRect(STAT_X + (STAT_BAR_WIDTH + GAP) * 3, STAT_Y + CLOUD_LINE_HEIGHT / 2, STAT_BAR_WIDTH, STAT_BAR_MAX_HEIGHT);
-  ctx.fillText('Игрок4', STAT_X + (STAT_BAR_WIDTH + GAP) * 3, STAT_Y + STAT_BAR_MAX_HEIGHT + CLOUD_LINE_HEIGHT * 1.5);
+  ctx.fillText('150', statX + (STAT_BAR_WIDTH + GAP) * 3, statY);
+  ctx.fillRect(statX + (STAT_BAR_WIDTH + GAP) * 3, statY + cloudLineHeight / 2, STAT_BAR_WIDTH, STAT_BAR_MAX_HEIGHT);
+  ctx.fillText('Игрок4', statX + (STAT_BAR_WIDTH + GAP) * 3, statY + STAT_BAR_MAX_HEIGHT + cloudLineHeight * 1.5);
 };
